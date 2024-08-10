f47be97b3d82330e80025aa483a945fec848c4a6
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-08-10 14:59:55 -0400

    Con repositorio

---
+++ src/main/java/example/micronaut/FrutaRepository.java
@@ -0,0 +1,14 @@
+package example.micronaut;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.data.mongodb.annotation.MongoRepository;
+import io.micronaut.data.repository.CrudRepository;
+
+import java.util.List;
+
+@MongoRepository // <1>
+public interface FrutaRepository extends CrudRepository<Fruta, String> {
+
+    @NonNull
+    Iterable<Fruta> findByNombreInList(@NonNull List<String> nombres); // <2>
+}
